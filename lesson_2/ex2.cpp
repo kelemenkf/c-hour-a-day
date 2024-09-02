fce3232822c16955fb74fcd97466e8900fd7c299
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-09-02 12:12:13 +0200

    Lesson 2

---
+++ lesson_2/ex2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// Function declaration
+int DemoConsoleOutput();
+
+int main()
+{
+    // Call i.e. invoke the function
+    DemoConsoleOutput();
+
+    return 0;
+}
+
+// Function definition
+int DemoConsoleOutput()
+{
+    cout << "10 - 5 = " << 10 - 5;
+    cout << "10 * 5 = " << 10 * 5;
+    cout << endl;
+    
+    return 0;
+}
---
+++ lesson_2/sandbox.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter two numbers: " << endl;
+    int number1;
+    int number2;
+    cin >> number1 >> number2;
+
+    cout << number1 << " " << number2 << endl;
+
+    return 0;
+}
+    
